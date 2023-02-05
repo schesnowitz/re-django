d69f9b805d5297b60dd9ef95203a6784e374a0db
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-05 14:52:20 -0500

    listing model route and list view

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    address = models.CharField(max_length=150)
+    square_footage = models.IntegerField()
+
+    def __str__(self):
+        return self.title
